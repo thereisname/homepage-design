cf678026887eb34f05d24afb72bc64806abb2f22
thereisname <79213859+thereisname@users.noreply.github.com>
2021-06-09 05:48:59 +0900

    Upgrade status-bar
    Summary:
    
    Changes all HTML file header.
    Upgrade status-bar.css.
    화면 창에 따라 status-bar의 창을 제어합니다.
    Create src/flex-control and src/gird-control.
    
    Reviewed By: thereisname

---
+++ js/status-bar.js
@@ -0,0 +1,34 @@
+const header = document.querySelector('header'),
+  offcanvasTop_btn = header.querySelector('#offcanvasTop'),
+  offcanvas_body = header.querySelector('.js-offcanvas-body'),
+  dropdown_menu = header.querySelector('.js-dropdown-menu');
+
+const SMALL = 640;
+const FLEX = 'flex';
+const OFFCANVAS = 'offcanvas';
+const OFFCANVAS_TOP = 'offcanvas-top';
+const OFFCANVAS_BODY = 'offcanvas-body';
+const DROPDOWN_MENU = 'dropdown-menu';
+const MARGIN = 'mg-r';
+
+function handleResize() {
+  const width = window.innerWidth;
+  if (width <= SMALL) {
+    offcanvasTop_btn.classList.add(OFFCANVAS, OFFCANVAS_TOP);
+    offcanvas_body.classList.add(OFFCANVAS_BODY);
+    dropdown_menu.classList.add(DROPDOWN_MENU);
+    dropdown_menu.classList.remove(FLEX, MARGIN);
+  } else {
+    offcanvasTop_btn.classList.remove(OFFCANVAS, OFFCANVAS_TOP);
+    offcanvas_body.classList.remove(OFFCANVAS_BODY);
+    dropdown_menu.classList.remove(DROPDOWN_MENU);
+    dropdown_menu.classList.add(FLEX, MARGIN);
+  }
+}
+
+function init() {
+  handleResize();
+  window.addEventListener('resize', handleResize);
+}
+
+init();
